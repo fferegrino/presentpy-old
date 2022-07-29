aa540048dfabb4d1ab9eaed7db2be3ed15d911ec
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-07-29 22:32:18 +0100

    Use click

---
+++ presentpy/__main__.py
@@ -0,0 +1,15 @@
+import click
+from pathlib import Path
+
+from presentpy.parser import process_notebook
+
+
+@click.command()
+@click.argument("file", type=click.Path(exists=True, dir_okay=False))
+def process(file):
+    path = Path(file)
+    presentation = process_notebook(path)
+    presentation.save(f"{path.stem}.pptx")
+
+if __name__ == '__main__':
+    process()
