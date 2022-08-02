c075e521a0b99f341ecf9680e82a5cda5b367d1d
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-08-02 06:56:04 +0100

    Use blank template

--- Makefile
+++ Makefile
@@ -1,3 +1,18 @@
 fmt:
 	black .
 	isort .
+
+build: pptx-templates
+	poetry build
+
+pptx-templates:
+	./utils/dopptx.sh templates
+	mkdir -p ./presentpy/templates
+	cp ./templates/*.pptx ./presentpy/templates/
+
+unpack-pptx:
+	./utils/unpptx.sh templates
+
+clean:
+	rm -rf dist
+	rm -rf .ipynb_checkpoints
--- utils/unpptx.sh
+++ utils/unpptx.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 function unpptx {
-    filename=$(basename -- $1)
-    parentdir="$(dirname "$1")"
+    parent_dir="$(dirname "$1")"
+    filename=$(basename -- "$1")
     extension=$2
-    echo "Decompressing $filename into $parentdir"
+    echo "Decompressing $filename into $parent_dir"
     source=$1
-    folder="$parentdir/${filename%.*}_${extension}"
+    folder="$parent_dir/${filename%.*}_${extension}"
 
-    unzip -qq -o -d $folder $source
+    unzip -qq -o -d "$folder" "$source"
 
-    find ./$folder -type f -name "*.xml" -exec  sh -c "cat {} | xmllint --format - | sponge {}" \;
-    find ./$folder -type f -name "*.rels" -exec  sh -c "cat {} | xmllint --format - | sponge {}" \;
+    find "$folder" -type f -name "*.xml" -exec sh -c "cat {} | xmllint --format - | sponge {}" \;
+    find "$folder" -type f -name "*.rels" -exec sh -c "cat {} | xmllint --format - | sponge {}" \;
 }
 
-for i in `find . -name "*.pptx" -type f`; do
+search_path="${1:-$PWD}"
+
+for i in $(find $search_path -name "*.pptx" -type f); do
     unpptx "$i" "pptx"
 done
 
-for i in `find . -name "*.potx" -type f`; do
+for i in $(find $search_path -name "*.potx" -type f); do
     unpptx "$i" "potx"
 done
